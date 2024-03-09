f501475cc3b0f9d114f1e0ab889f6aab19b84938
Renert, Mila (lr2827) <lr2827@us.att.com>
2024-03-09 13:06:23 -0600

    First commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route('/login', methods=['POST'])
+def login():
+    # Assuming the POST request contains JSON data with keys 'username' and 'password'
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+
+    # Perform authentication logic here
+    # For demonstration purposes, let's just return a success message
+    if username == 'admin' and password == 'admin':
+        return jsonify({'ok': True})
+    else:
+        return jsonify({'ok': False}), 401
+
+@app.route('/hello', methods=['GET'])
+def sayHello():
+	print("Got into sayHello function")
+	return jsonify({'message': 'Hello'})
+	
+if __name__ == '__main__':
+    app.run(port=3000)
