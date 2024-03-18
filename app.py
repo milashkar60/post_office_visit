2061dd8f8c5f53e0d41a2b9c4a74b466600d5303
Renert, Mila (lr2827) <lr2827@us.att.com>
2024-03-18 10:46:05 -0500

    added function

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import json
+import boto3
+import requests
 
 
 app = Flask(__name__)
@@ -21,13 +24,22 @@ def login():
 	
 @app.route('/connect', methods=['POST'])
 def connectToModel():
-	print("Do something here to connect to model")
-    
-    # add code here to connect to AI model
-    # return a JSON object with key 'advice' and 'value' whatever you is returned from the model
-
-	return jsonify({
-        'advice': 'slkdjslkfjsljfdslkjfflk'
+	# Connect to model
+    url = "https://mgs9tq1g7f.execute-api.us-east-1.amazonaws.com/MellissaStage/Get"  
+ 
+    # Make a GET request
+    response = requests.get(url)
+ 
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+    # Access the response data
+        data = response.json()  # Assuming the response is JSON data
+        print(data)
+    else:
+        print("Error:", response.status_code)    
+        
+    return jsonify({
+        'advice': data["body"]
     })
 	
 if __name__ == '__main__':
